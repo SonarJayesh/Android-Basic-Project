611fdc474e89a21fdef0a8cbef370b92bf2fd1fa
SonarJayesh <jayeshsonar2824@gmail.com>
2025-10-30 23:33:36 +0530

    [ADDED]: JAYESH This is Simple MenuOption demo code for selecting the options using menulist in android

---
+++ MenuOption/app/src/main/java/com/jayesh/menuoption/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jayesh.menuoption;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+    public void optionMenu(View v) {
+        startActivity(new Intent(MainActivity.this,OptionMenuActivity.class));
+    }
+}
